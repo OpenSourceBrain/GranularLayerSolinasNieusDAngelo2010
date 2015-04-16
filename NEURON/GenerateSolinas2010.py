eb44773553f0541f0d099eeed5be99d62c3b9f10
Sergio Solinas <theiera@gmail.com>
2015-04-16 19:55:10 +0200

    Bugfix: now NEURON sees the recently compiled x86_64 dir.

--- NEURON/GenerateSolinas2010.py
+++ NEURON/GenerateSolinas2010.py
@@ -3,15 +3,18 @@ PYTHON script to run the network structure generation
 used in the Solinas et al. 2010
 and load it into a PYTHON dictionary
 """
-from neuron import h
 import load_structure as loader
 import os
+import ipdb
 
 def generate_solinas2010():
     # Load a shor version of the Start.hoc file
     # that does not run the sim
     # it saves the network structure to the Sim_data dir
 
+    if not os.path.exists('SimData'):
+        os.mkdir('SimData')
+        
     h.load_file('Start_test.hoc')
 
 def compile_mods():
@@ -20,5 +23,6 @@ def compile_mods():
     
 if __name__ == '__main__':
     compile_mods()
+    from neuron import h
     generate_solinas2010()
     structure = loader.load_Solinasetal2010_structure()
